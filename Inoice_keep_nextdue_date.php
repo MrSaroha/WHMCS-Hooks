7fe6d5e6c602e5c11b1b59005db30af98e8e39b4
MrSaroha <64730969+MrSaroha@users.noreply.github.com>
2024-12-10 16:11:34 +0530

    Comment logs

--- Inoice_keep_nextdue_date.php
+++ Inoice_keep_nextdue_date.php
@@ -16,21 +16,17 @@ if (!defined("WHMCS"))
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Carbon\Carbon;
 
-function nhgroup_add_log($log_msg){
-    //  $nn = $data.$final_url;
-    // $file = fopen("testLOG.txt","w");
-    // echo fwrite($file,$nn);
-    // fclose($file);
+function nhgroup_add_log($log_msg){    
     $txt = "\n".$log_msg;
     $myfile = file_put_contents('testLOG.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 }
 function override_next_due_date($table,$id,$date){
-     nhgroup_add_log("due date function called");
+    //nhgroup_add_log("due date function called");
     $data = Capsule::table($table)->where('id',$id)->update(array('nextduedate'=>$date,'nextinvoicedate'=>$date));
     return true;
 };
 add_hook('InvoicePaid', 1, function($vars) {
-     nhgroup_add_log("Hooks called");
+    //nhgroup_add_log("Hooks called");
     $command = 'GetInvoice';
     $postData = array(
         'invoiceid' => $vars['invoiceid'],
@@ -40,16 +36,16 @@ add_hook('InvoicePaid', 1, function($vars) {
 
     $invoice_due_date = $results['duedate'];
     $m = "invoice id =".$vars['invoiceid']." and duedate ".$invoice_due_date;
-     nhgroup_add_log($m);
+    //nhgroup_add_log($m);
     foreach($results['items']['item'] as $item){
         if($item['type']=="Hosting"){
-             nhgroup_add_log("In hosting option");
+             //nhgroup_add_log("In hosting option");
             override_next_due_date("tblhosting",$item['relid'],$invoice_due_date);
         }elseif(strpos($item['type'], "omain")>0){
             override_next_due_date("tbldomains",$item['relid'],$invoice_due_date);
         }
         $ms = "in loop type=".$item['type'];
-         nhgroup_add_log($ms);
+         //nhgroup_add_log($ms);
 
     }
 });
